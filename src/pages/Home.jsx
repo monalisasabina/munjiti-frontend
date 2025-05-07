986d18ed33eb9b0232c2710fec9f937ad8f5a746
Monalisa Sabina <onyangomonalisa@hotmail.com>
2025-05-07 18:56:42 +0300

    working on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,20 @@
+import "./home.css"
+// import churchImage from '../Pictures/church images.jpg'
+
 function Home(){
 
  return(
-    <>
-       <h1> NYUMBANI</h1>
-    </>
+    <div className="home_cont">
+
+       <h1 className="home_title"> Welcome to St. Mary's Munjiti Church</h1>
+
+       <div className="home_services_cont">
+
+           <h4 className="home_services_title"> OUR SERVICES</h4>
+
+       </div>
+
+    </div>
 
  )
 }
