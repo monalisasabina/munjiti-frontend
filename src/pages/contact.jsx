c250d67092bd06684edfbad2295ae08f980d5abf
Monalisa Sabina <onyangomonalisa@hotmail.com>
2025-05-09 16:49:05 +0300

    working on contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,9 +1,52 @@
+import './contact.css'
+
 function ContactUs(){
 
     return(
-        <>
-          <h1>CONTACT US</h1>
-        </>
+        <div className='contact_cont'>
+
+           <div className='contact_title_cont'>
+              <h1 className='contact_title'>CONTACT US</h1>
+           </div>
+
+           <div className='contact_form_cont'>
+
+              <form>
+                   {/* First Name */}
+                   <label htmlFor='fname'>First Name</label>
+                   <input type='text'
+                          className='contact_fname' 
+                          placeholder='Your First Name...' />
+
+                   {/* Last Name */}
+                   <label htmlFor='lname'>Last Name</label>
+                   <input type='text'
+                          className='contact_lname' 
+                          placeholder='Your Last Name...' />
+
+                    {/* Email Number */}
+                    <label htmlFor='email'>Email</label>
+                    <input type='email'
+                          className='contact_email' 
+                          placeholder='Your Email...' />
+
+                    {/* Message */}
+                    <label htmlFor='fname'>First Name</label>
+                    <textarea className='contact_message'
+                              name='message'
+                              placeholder=''
+                    
+                    ></textarea>
+
+
+
+
+
+              </form>
+
+           </div>
+           
+        </div>
     )
 }
 
