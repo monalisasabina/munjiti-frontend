fe7dc29cfb0c546f8c2e10ec67c079e3cc0205f0
Monalisa Sabina <onyangomonalisa@hotmail.com>
2025-05-12 14:40:28 +0300

    working on contact page form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -7,36 +7,43 @@ function ContactUs(){
 
            <div className='contact_title_cont'>
               <h1 className='contact_title'>CONTACT US</h1>
+
+              <div className='other_contacts'>
+                 <h3> Mobile No: </h3>
+              </div>
+
+ 
+
            </div>
 
            <div className='contact_form_cont'>
 
-              <form>
+              <form className='contact_form'>
                    {/* First Name */}
                    <label htmlFor='fname'>First Name</label>
                    <input type='text'
-                          className='contact_fname' 
-                          placeholder='Your First Name...' />
+                          className='contact_input'  />
 
                    {/* Last Name */}
                    <label htmlFor='lname'>Last Name</label>
                    <input type='text'
-                          className='contact_lname' 
-                          placeholder='Your Last Name...' />
+                          className='contact_input' />
 
                     {/* Email Number */}
                     <label htmlFor='email'>Email</label>
                     <input type='email'
-                          className='contact_email' 
-                          placeholder='Your Email...' />
+                          className='contact_input' 
+                           />
 
                     {/* Message */}
-                    <label htmlFor='fname'>Message</label>
+                    <label htmlFor='message'>Message</label>
                     <textarea className='contact_message'
                               name='message'
                               placeholder=''
-                    
                     ></textarea>
+
+                    {/* Submit button */}
+                    <button className='contact_button'> SUBMIT</button>
 
 
 
