fee399a03fd3e0d3cbfa563e3da66cc177fdc0b0
Monalisa Sabina <onyangomonalisa@hotmail.com>
2025-08-27 18:02:36 +0300

    working on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,59 +3,49 @@ import churchImage from '../Pictures/Munjiti Church.png'
 
 function Home(){
 
+   const serviceTimes = [
+      { title: "Early Morning Daily Service", time: "5:30am-6:30am" },
+      { title: "Sunday School", time: "8:00am-9:00am" },
+      { title: "English Sunday service", time: "9:00am-10:30am" },
+      { title: "Kiswahili Sunday service (Main)", time: "10:30am-12:30pm" },
+      { title: "Compassion Saturday Programme", time: "from 7:00am" },
+   ];
+
  return(
     <div className="home_cont">
 
        <div className="home_intro">
 
-          <h1 className="home_title"> Welcome to St. Mary's Munjiti Parish</h1>
+         <div className="home-heading-block">
 
-           <blockquote className="give_verse">
-               But seek first his kingdom and his righteousness, and all these things will be given to you as well.
-                <cite className="verse_ref">Matthew 6:33</cite>
-           </blockquote>
+             <h1 className="home_title"> Welcome to St. Mary's Munjiti Parish</h1>
+
+             <blockquote className="give_verse">
+               <p>But seek first his kingdom and his righteousness, and all these things will be given to you as well</p>
+               <cite className="verse_ref">-Matthew 6:33</cite>
+             </blockquote>
           
+         </div>
 
           <div className="home_images_cont">
-               <img className="home_image"  src={churchImage} alt="church image"/>
+               <img className="home_image"  src={churchImage} alt="St. Mary's Munjiti Church building"/>
           </div>
          
         </div>
 
        <div className="home_services_cont">
 
-           <h4 className="home_services_title"> OUR SERVICES</h4>
-
-          
+           <h2 className="home_services_title"> OUR SERVICES</h2>
 
            <div className="home_services_time_cont">
 
-               <div>
-                 <p className="service_title"> Early Morning Daily Service</p>
-                 <p className="service_time"> 5:30am-6:30am</p>
-               </div>
-
-                <div>
-                 <p className="service_title"> Compassion Saturday Programme</p>
-                 <p className="service_time"> from 7:00am</p>
-               </div>   
-
-               <div>
-                  <p className="service_title">Sunday School</p>
-                  <p className="service_time">8:00am-9:00am</p>
-               </div>
-
-               <div>
-                  <p className="service_title">English Sunday service</p>
-                  <p className="service_time">9:00am-10:30am</p>
-               </div>
-
-               <div>
-                  <p className="service_title">Kiswahili Sunday service (Main) </p>
-                  <p className="service_time">10:30am-12:30pm</p>
-               </div>
+               {serviceTimes.map((service, index) => (
+                   <div key={index}>
+                       <p className="service_title">{service.title}</p>
+                       <p className="service_time">{service.time}</p>
+                   </div>
+               ))}
            </div>
-
        </div>
 
     </div>
